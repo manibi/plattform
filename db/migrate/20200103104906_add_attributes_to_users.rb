1d102928d8f484487c6e016b725b494a20e1e5ee
Cristian Cristea <cristiancristea.1987@gmail.com>
2020-01-05 15:57:03 +0100

    Replace exam_day with exam_date, fix tests

--- db/migrate/20200103104906_add_attributes_to_users.rb
+++ db/migrate/20200103104906_add_attributes_to_users.rb
@@ -2,6 +2,6 @@ class AddAttributesToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :first_name, :string
     add_column :users, :birth_date, :date
-    add_column :users, :exam_day, :date
+    add_column :users, :exam_date, :date
   end
 end
--- db/migrate/20200105143933_rename_exam_day_to_exam_date.rb
+++
@@ -1,5 +0,0 @@
-class RenameExamDayToExamDate < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :users, :exam_day, :exam_date
-  end
-end
