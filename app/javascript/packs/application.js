01806f11a3ad45ef71e14de62b58a35ae1be088a
Cristian Cristea <cristiancristea.1987@gmail.com>
2020-01-22 10:19:37 +0100

    Remove jquery import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-require("jquery");
+// require("jquery");
 
 // libraries
 import Sortable from "sortablejs";
