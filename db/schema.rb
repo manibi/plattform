0f6731ae1add4a05fde2f94768ba69cb12cba1c9
Cristian Cristea <cristiancristea.1987@gmail.com>
2020-02-11 12:29:20 +0100

    Add explanation to flashcard answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_153205) do
+ActiveRecord::Schema.define(version: 2020_02_11_112635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_02_10_153205) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "explanation"
   end
 
   create_table "articles", force: :cascade do |t|
