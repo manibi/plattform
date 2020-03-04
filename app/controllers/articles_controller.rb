8022b65ff754bcdca11425575e5394d76794458f
manibi <manibi@web.de>
2020-03-04 14:06:41 +0100

    last chapter edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,9 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+    @article.chapters.build(title: "Erläuterung")
+    @article.chapters.build(title: "Praxisbeispiel")
+    @article.chapters.build(title: "Verwandte Themen")
     authorize @article
     @categories = current_user.all_categories
   end
