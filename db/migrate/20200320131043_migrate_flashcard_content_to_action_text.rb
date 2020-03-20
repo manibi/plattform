06d8ff2b952cc673753f4e3e953c882d8645f43b
Cristian Cristea <cristiancristea.1987@gmail.com>
2020-03-20 15:12:18 +0100

    Migrate flashcard content to action text, style article author page

--- db/migrate/20200320131043_migrate_flashcard_content_to_action_text.rb
+++ db/migrate/20200320131043_migrate_flashcard_content_to_action_text.rb
@@ -1,12 +1,10 @@
 class MigrateFlashcardContentToActionText < ActiveRecord::Migration[6.0]
+  include ActionView::Helpers::TextHelper
   def change
-    include ActionView::Helpers::TextHelper
-    def change
-      rename_column :flashcards, :content, :content_old
-      Flashcard.all.each do |flashcard|
-        flashcard.update_attribute(:content, simple_format(flashcard.content_old))
-      end
-      remove_column :flashcards, :content_old
+    rename_column :flashcards, :content, :content_old
+    Flashcard.all.each do |flashcard|
+      flashcard.update_attribute(:content, simple_format(flashcard.content_old))
     end
+    remove_column :flashcards, :content_old
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_132733) do
+ActiveRecord::Schema.define(version: 2020_03_20_131043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -146,7 +146,6 @@ ActiveRecord::Schema.define(version: 2020_03_17_132733) do
   end
 
   create_table "flashcards", force: :cascade do |t|
-    t.text "content"
     t.string "flashcard_type"
     t.text "correct_answers"
     t.bigint "article_id", null: false
