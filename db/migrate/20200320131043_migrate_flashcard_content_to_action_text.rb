fbe4af6f353d6dcdf06fd6717c4d97b6d7773c0b
Cristian Cristea <cristiancristea.1987@gmail.com>
2020-03-20 14:12:24 +0100

    Migrate to action text

---
+++ db/migrate/20200320131043_migrate_flashcard_content_to_action_text.rb
@@ -0,0 +1,12 @@
+class MigrateFlashcardContentToActionText < ActiveRecord::Migration[6.0]
+  def change
+    include ActionView::Helpers::TextHelper
+    def change
+      rename_column :flashcards, :content, :content_old
+      Flashcard.all.each do |flashcard|
+        flashcard.update_attribute(:content, simple_format(flashcard.content_old))
+      end
+      remove_column :flashcards, :content_old
+    end
+  end
+end
